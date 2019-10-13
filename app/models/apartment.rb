ad906fc1345068940c37eac6a0fd9ad5c7b2d91f
Shane Poncik <sbponcik@gmail.com>
2019-10-13 10:42:56 -0500

    database models update
    converted review.metrics to a hash(metric->rating), added extra seed information, added a couple more helper functions for models to retrieve foreign key models

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,6 @@
 class Apartment < ApplicationRecord
+  def get_reviews
+    reviews = Review.where(:apartment_id => self.id)
+    return reviews
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def get_reviews
+    return Review.where(:user_id => self.id)
+  end
 end
