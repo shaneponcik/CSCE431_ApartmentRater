eb6d5bb4bad331cadaf44f53ea732148a5501a72
Dawson <dfields16@tamu.edu>
2019-10-13 19:04:27 -0500

    Add User login/creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_165009) do
+ActiveRecord::Schema.define(version: 2019_10_13_203228) do
 
   create_table "amenities", force: :cascade do |t|
     t.string "name"
@@ -77,8 +77,7 @@ ActiveRecord::Schema.define(version: 2019_10_10_165009) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.boolean "verified"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
