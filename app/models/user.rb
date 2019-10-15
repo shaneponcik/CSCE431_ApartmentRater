4e5ea3f0448e652047d3044400fcbf50fb6e864e
EC2 Default User <ec2-user@ip-172-31-18-180.us-east-2.compute.internal>
2019-10-15 16:29:42 +0000

    Added function to get reviews of a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  
+  def get_reviews
+    reviews = Review.where(:user_id => self.id)
+    return reviews
+  end
 
 end
