8d469fa6b541cd1ef6b11c61abb58f86fb83ecd7
Shane Poncik <sbponcik@gmail.com>
2019-10-29 17:34:29 -0500

    Add review, edit review, destroy review

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,4 +1,3 @@
 class Amenity < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-
 end
--- app/models/review_tag.rb
+++ app/models/review_tag.rb
@@ -1,5 +1,5 @@
 class ReviewTag < ApplicationRecord
   validates :review_id, presence: true
   validates :tag_id, presence: true
-
+  validates :review_id, uniqueness: { scope: :tag_id }
 end
