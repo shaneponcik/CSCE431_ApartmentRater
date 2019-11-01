deb4968645e49114cdbd18cab20a445febc0f16c
JHannTX <jhanntx@gmail.com>
2019-10-31 23:46:13 -0500

    Have a working form with an updated list of amenities

--- app/controllers/amenities_controller.rb
+++ app/controllers/amenities_controller.rb
@@ -69,6 +69,6 @@ class AmenitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def amenity_params
-      params.fetch(:amenity, {})
+      params.require(:amenity).permit(:name)
     end
 end
