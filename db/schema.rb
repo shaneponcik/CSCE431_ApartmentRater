1e86d161e54527ed648fb0d694f575e678431065
JHannTX <jhanntx@gmail.com>
2019-11-07 22:58:42 -0600

    Ran the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_142350) do
+ActiveRecord::Schema.define(version: 2019_11_08_043823) do
 
   create_table "admin_tables", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2019_11_07_142350) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.decimal "bus_route"
+    t.boolean "near_campus"
+    t.string "building_near"
     t.index ["name"], name: "index_apartments_on_name", unique: true
   end
 
