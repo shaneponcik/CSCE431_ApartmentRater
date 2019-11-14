5243047da1a65e12c030dcfd74d486f5694f3c24
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2019-11-14 09:29:20 -0600

    Add files via upload

---
+++ features/step_definitions/add_admin_portal_steps.rb
@@ -0,0 +1,15 @@
+When(/^When I am on the apartment list page$/) do
+  
+end
+
+Then(/^$/) do
+  
+end
+
+When(/^$/) do
+  
+end
+
+Then(/^I should see a success message for the amenity$/) do
+  
+end
