507ac7184b50eb20d7fb0a209a7fec3363d384a5
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2019-11-14 12:09:04 -0600

    Update add_admin_portal_steps.rb

--- features/step_definitions/add_admin_portal_steps.rb
+++ features/step_definitions/add_admin_portal_steps.rb
@@ -1,15 +1,35 @@
-When(/^When I am on the apartment list page$/) do
+When(/^When I am on the apartment list page and logged out$/) do
   
 end
 
-Then(/^$/) do
+Then(/^I attempt to access admin portal$/) do
   
 end
 
-When(/^$/) do
+Then(/^I get redirected to the root url$/) do
   
 end
 
-Then(/^I should see a success message for the amenity$/) do
+When(/^When I am on the apartment list page and logged in as an admin$/) do
+  
+end
+
+Then(/^I attempt to access admin portal$/) do
+  
+end
+
+Then(/^I get to access the admin portal$/) do
+  
+end
+
+When(/^When I am on the apartment list page and logged in as a non-admin user$/) do
+  
+end
+
+Then(/^I attempt to access admin portal$/) do
+  
+end
+
+Then(/^I get redirected to the root url$/) do
   
 end
