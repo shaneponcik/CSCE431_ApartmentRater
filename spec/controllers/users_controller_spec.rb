76cfe25ce92f2886455bf2a406f2dd69c59f574f
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2019-11-14 20:47:19 -0600

    Update users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe UsersController, type: :controller do
     it "returns a success response" do
       User.create! valid_attributes
       get :index, params: {}, session: valid_session
-      expect(response).to be_successful
+      expect(response).to not be_successful
     end
   end
 end
