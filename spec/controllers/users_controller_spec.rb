d41b3dcd7b479f0e87966a67b91aae950d7b6eb4
ColbyMainard <55711813+ColbyMainard@users.noreply.github.com>
2019-11-14 20:46:15 -0600

    Update users_controller_spec.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe AdminTableController, type: :controller do
+RSpec.describe UsersController, type: :controller do
+  # This should return the minimal set of attributes required to create a valid
+  # Tag. As you add validations to Tag, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+  }
 
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+
+  # This should return the minimal set of values that should be in the session
+  # in order to pass any filters (e.g. authentication) defined in
+  # TagsController. Be sure to keep this updated too.
+  let(:valid_session) { {} }
+
+  describe "GET users#admin_portal" do
+    it "returns a success response" do
+      User.create! valid_attributes
+      get :index, params: {}, session: valid_session
+      expect(response).to be_successful
+    end
+  end
 end
