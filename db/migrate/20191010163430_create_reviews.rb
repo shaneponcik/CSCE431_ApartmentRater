5582696da75bc51de09b5617caf77b98c886c788
Shane Poncik <sbponcik@gmail.com>
2019-12-07 05:05:37 -0600

    fixed migration from adding double key

--- db/migrate/20191010163430_create_reviews.rb
+++ db/migrate/20191010163430_create_reviews.rb
@@ -1,14 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.references :apartment, index:true, foreign_key:true
-      t.references :user, index:true, foreign_key:true
+      t.references :apartment, index:true, foreign_key: {on_delete: :cascade}
+      t.references :user, index:true, foreign_key: {on_delete: :cascade}
       t.text :review_text
       t.decimal :price, :default => 0.00
       t.timestamps
     end
-
-    add_foreign_key :reviews, :apartments, on_delete: :cascade
-    add_foreign_key :reviews, :users, on_delete: :cascade
   end
 end
--- db/migrate/20191010165009_create_review_metrics.rb
+++ db/migrate/20191010165009_create_review_metrics.rb
@@ -1,13 +1,10 @@
 class CreateReviewMetrics < ActiveRecord::Migration[6.0]
   def change
     create_table :review_metrics do |t|
-      t.references :review, index:true, foreign_key:true
-      t.references :metric, index:true, foreign_key: true
+      t.references :review, index:true, foreign_key:{on_delete: :cascade}
+      t.references :metric, index:true, foreign_key: {on_delete: :cascade}
       t.integer :rating
       t.timestamps
     end
-
-    add_foreign_key :review_metrics, :reviews, on_delete: :cascade
-    add_foreign_key :review_metrics, :metrics, on_delete: :cascade
   end
 end
